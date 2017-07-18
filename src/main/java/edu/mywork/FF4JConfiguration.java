db0df7b650b6f9522ac6fa1e90709ca863ed4c1f
prasegaonkar <prashant.rasegaonkar@gmail.com>
2017-07-18 21:20:07 +0530

    back to springboot app and added spring-jdbc

--- src/main/java/edu/mywork/FF4JConfiguration.java
+++ src/main/java/edu/mywork/FF4JConfiguration.java
@@ -1,13 +1,27 @@
 package edu.mywork;
 
+import javax.sql.DataSource;
+
 import org.ff4j.FF4j;
+import org.ff4j.cache.InMemoryCacheManager;
+import org.ff4j.store.JdbcFeatureStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FF4JConfiguration {
-    @Bean
-    public FF4j getFF4j() {
-        return new FF4j("ff4j-features.xml");
-    }
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Bean
+	public FF4j getFF4j() {
+
+		FF4j ff4j = new FF4j();
+		ff4j.setFeatureStore(new JdbcFeatureStore(dataSource));
+		ff4j.cache(new InMemoryCacheManager());
+
+		return ff4j;
+	}
 }
